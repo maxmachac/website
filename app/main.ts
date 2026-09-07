56eb4fe2295b1560acda470291722b9652007d64
maxmachac <max.machac@icloud.com>
2026-09-07 19:11:36 +0200

    fixing github pages

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,5 @@
 // Global code
-import '../styles/app.scss';
+import '../styles/app.css';
 import flamethrower from 'flamethrower-router';
 import { scrollSave } from './util/scroll';
 import './util/key-bindings';
